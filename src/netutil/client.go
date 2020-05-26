9598eca0328999f4364b4083c881c07093cf2973
xuqiuyang <1556147829@qq.com>
2020-05-26 15:51:59 +0800

    create client.go

---
+++ src/netutil/client.go
@@ -0,0 +1,36 @@
+package netutil
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"net"
+	"time"
+)
+
+func demo() {
+	if len(os.Args) <= 1 {
+		fmt.Println("usage:go run clent.go need Your Content")
+		return
+	}
+
+	log.Println("begin dial")
+
+	conn, err := net.Dial("tcp", ":3306")
+
+	if err != nil {
+		log.Println("dail error", err)
+		return
+	}
+
+	defer conn.Close()
+	log.Println("dail ok")
+
+	time.Sleep(time.Second * 2)
+
+	data := os.Args[1]
+	// data := make([]byte, 65536)
+	conn.Write([]byte(data))
+
+	time.Sleep(time.Second * 5)
+}
